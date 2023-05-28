5d5c82aae13e0cc93de270fcf1cfdea52b72ab39
2977817275 <2977817275@qq.com>
2023-05-29 00:19:41 +0800

    2023.5.29
    增加了一点细节，增加了注册时候的细节

--- src/login-register.js
+++ src/login-register.js
@@ -6,6 +6,9 @@ let register=document.getElementById('btn_register');
 let form_box=document.getElementsByClassName('form-box')[0];
 let register_box=document.getElementsByClassName('register-box')[0];
 let login_box=document.getElementsByClassName('login-box')[0];
+let username = document.getElementById('username');
+let psd = document.getElementById('password');
+let rpsd = document.getElementById('re_password');
 // 去注册按钮点击事件
 register.addEventListener('click',()=>{
     form_box.style.transform='translateX(90%)';
@@ -18,3 +21,27 @@ login.addEventListener('click',()=>{
     register_box.classList.add('hidden');
     login_box.classList.remove('hidden');
 })
+function checkForm() {
+    if (username.value == "") {
+        username.style.border = "1px solid rgba(197,81,58,0.8)";
+        alert("用户名不能为空！");
+        return;
+    } else {
+        username.style.border = " 1px solid rgba(255,255,255,0.3)";
+    }
+    if (rpsd.value == '' || psd.value == '') {
+        alert("密码不能为空！");
+        return;
+    }
+    if (psd.value.length < 6) {
+        alert("密码不能小于6位!");
+        return;
+    }
+    if (rpsd.value == psd.value) {
+        alert("注册成功！")
+        btn_login.click();
+    } else {
+        alert("密码不一致！");
+        rpsd.value = "";
+    }
+}
